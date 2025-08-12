dab061cb6dbbd2b0e7f475a1d6b74994a1e7febc
Matias Corbalan <matt.corbalan@gmail.com>
2025-08-12 15:17:39 -0300

    correccion firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC09XDKt0b8Em6VssUQl11hm32ua8Z57OI",
     authDomain: "tiendamatias-db.firebaseapp.com",
     projectId: "tiendamatias-db",
     storageBucket: "tiendamatias-db.firebasestorage.app",
     messagingSenderId: "658103762093",
     appId: "1:658103762093:web:1b3e5ccbe50af5b53f5a24"
 };
+
 
 export const app = initializeApp(firebaseConfig);
